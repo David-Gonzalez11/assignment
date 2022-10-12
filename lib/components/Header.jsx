2cef5e7041326323acc6b69b54ee904cd88275c3
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-10-12 23:55:30 +0000

    In progress

---
+++ lib/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Header = () => {
+  return (
+    <header className="container-lg">
+      <p className="text-center fs-5">Todo list</p>
+    </header>
+  );
+};
+export default Header;
